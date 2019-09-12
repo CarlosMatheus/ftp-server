0e8d4c8169816a372e439cb4bd7c0765690a183c
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-09-12 13:18:33 -0300

    Initiate utils

---
+++ main.py
@@ -0,0 +1,7 @@
+from server import Server
+from client import Client
+
+server = Server()
+client = Client()
+
+client.send_test_message()
---
+++ utils.py
@@ -0,0 +1,2 @@
+DEFAULT_ADDRESS = ('0.0.0.0', 8080)
+COMMAND_LIST = ['cd', 'ls', 'pwd', 'mkdir', 'rmdir', 'get', 'put', 'delete', 'close', 'open', 'quit']
